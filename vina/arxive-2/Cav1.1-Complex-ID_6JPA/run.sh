a421baf2338207dfb866e69f82ff3a2a5a30c032
ilosrim <ilosrim@yahoo.com>
2023-01-04 14:43:42 +0500

    Completed docking

--- vina/arxive-2/Cav1.1-Complex-ID_6JPA/run.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/bash
-
-for f in ./ligands/ligand_*.pdbqt; do
-	b=`basename $f .pdbqt`
-	echo Proccessing ligand $b
-	mkdir -p ./dist/$b
-	vina --config conf.txt --ligand $f --out ./dist/${b}/out.pdbqt --log ./dist/${b}/log.txt
-done
